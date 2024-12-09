ecb741370cee01c85d93dfffef6a2d0c9ea64b52
Othon Felipe <ofbsantos@gmail.com>
2024-12-09 00:21:43 -0300

    Refatora o estilo inline do `index` para um arquivo de estilo `/styles/global.css`

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css"; // Importando o CSS do Bootstrap
 import "slick-carousel/slick/slick.css"; // Importando o CSS do slick-carousel
 import "slick-carousel/slick/slick-theme.css"; // Importando o tema do slick-carousel
 import "../../styles/custom.css"; // Importa o estilo de customizações
+import "../../styles/global.css"; // Importa o estilo inline
 
 import { useEffect } from "react";
 import Slider from "react-slick"; // Importando o componente Slider do slick
